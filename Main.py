18c1844e7856c5133311790ecba37c5924bdc7d5
divwajidali <dev.wajidali@gmail.com>
2026-05-16 11:54:53 +0500

    login system

--- Main.py
+++ Main.py
@@ -3,81 +3,140 @@ class Authentication:
         pass
 
     def signup(self, name , email, password, role):
-        self.name = name
-        self.email = email
-        self.role = role
-        self.password = password
+        
         self.info = {}
         self.info.update({"Name" : name})
         self.info.update({"Email" : email})
         self.info.update({"Password" : password})
         if (role == "Admin"):
-                self.info.update({"Role" : role})
-                self.info = str(self.info)
-                with open("E:\GitDemo\Smart_College_Management_System\Admin.txt", "a") as f:
-                    f.write(self.info)
+            self.info.update({"Role" : role})
+            self.info = str(self.info)
+            with open("E:\GitDemo\Smart_College_Management_System\Admin.txt", "a") as f:
+                f.write(self.info)
 
         elif (role == "Teacher"):
-                self.info.update({"Role" : role})
-                self.info = str(self.info)
-                with open("E:\GitDemo\Smart_College_Management_System\Teacher.txt", "a") as f:
-                    f.write(self.info)
+            self.info.update({"Role" : role})
+            self.info = str(self.info)
+            with open("E:\GitDemo\Smart_College_Management_System\Teacher.txt", "a") as f:
+                f.write(self.info)
 
         elif (role == "Student"):
-                self.info.update({"Role" : role})
-                self.info = str(self.info)
-                with open("E:\GitDemo\Smart_College_Management_System\Student.txt", "a") as f:
-                    f.write(self.info)
+            self.info.update({"Role" : role})
+            self.info = str(self.info)
+            with open("E:\GitDemo\Smart_College_Management_System\Student.txt", "a") as f:
+                f.write(self.info)
 
+    def login(self, email, password, role):
+        
+
+        if (role == "Admin"):
+            with open("E:\GitDemo\Smart_College_Management_System\Admin.txt", "r") as f:
+                self.data = f.read()
+                if email in self.data:
+                    if password in self.data:
+                        print("Login Successfully.")
+                    else:
+                        print("Invalid user name or password.")
+                else:
+                    print("Invalid user name or password.")
+
+        elif (role == "Teacher"):
+            with open("E:\GitDemo\Smart_College_Management_System\Teacher.txt", "r") as f:
+                self.data = f.read()
+                if email in self.data:
+                    if password in self.data:
+                        print("Login Successfully.")
+                    else:
+                        print("Invalid user name or password.")
+                else:
+                    print("Invalid user name or password.")
+
+        elif (role == "Student"):
+            with open("Smart_College_Management_System/Student.txt", "r") as f:
+                self.data = f.read()
+                if email in self.data:
+                    if password in self.data:
+                        print("Login Successfully.")
+                    else:
+                        print("Invalid user name or password.")
+                else:
+                    print("Invalid user name or password.")
+        
 
 c1 = Authentication()
-choice = input("1. Singup\n2. Login\nEnter Choice :")
+while True:
+    choice = input("1. Singup\n2. Login\nEnter Choice :")
 
-if (choice == "1"):
+    if (choice == "1"):
     
    
-    name = input("Enter Name :")
+        name = input("Enter Name :")
+        try :
+            name = name.title()
+
+        except ValueError:
+            pass
     
-    while True:
+        while True:
+            email = input("Enter Email :")
+            if (email.endswith("@gmail.com")):
+            
+                break
+            else:
+                print("You email is incorret.\nPlease enter email again.")
+    
+        while True:
+            password = input("Enter Password :")
+            if (len(password) < 8):
+                print("Your password is less than 8 character.\nPlease enter password again.")
+    
+
+            else:
+            
+                break
+
+        while True:
+            role = input("Enter Role :")
+            try:
+                role = role.title()
+
+                if (role == "Admin"):
+                
+                    break
+
+                elif (role == "Teacher"):
+                
+                    break
+
+                elif(role == "Student"):
+                
+                    break
+
+                else:
+                    print("You enter invalid role.\nPlease enter role again.")
+            except ValueError:
+                print("You enter invalid role.\nPlease enter role again.")
+
+
+        c1.signup(name, email, password, role)
+        print("Signup Successfully.")
+        break
+
+
+
+    elif (choice == "2"):
+
         email = input("Enter Email :")
-        if (email.endswith("@gmail.com")):
-            
-            break
-        else:
-            print("You email is incorret.\nPlease enter email again.")
-    
-    while True:
         password = input("Enter Password :")
-        if (len(password) < 8):
-            print("Your password is less than 8 character.\nPlease enter password again.")
-    
-
-        else:
-            
-            break
-
-    while True:
         role = input("Enter Role :")
         try:
             role = role.title()
-
-            if (role == "Admin"):
-                
-                break
-
-            elif (role == "Teacher"):
-                
-                break
-
-            elif(role == "Student"):
-                
-                break
-
-            else:
-                print("You enter invalid role.\nPlease enter role again.")
         except ValueError:
-            print("You enter invalid role.\nPlease enter role again.")
+            pass
+     
+        c1.login(email, password, role)
+        break
 
-
-    c1.signup(name, email, password, role)
-    print("Signup Successfully.")
+    else:
+        print("You entered an invalid option.\nPlease enter valid option.")
+    
